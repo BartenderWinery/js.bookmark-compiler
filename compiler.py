62a100b41a54a70e49eacc55f1602ae0a5162411
Bartender <66457110+BartenderWinery@users.noreply.github.com>
2022-05-27 16:22:33 -0400

    lazy code, ill fix it later

--- compiler.py
+++ compiler.py
@@ -1,5 +1,11 @@
-import sys,os
+import sys,re
 with open(sys.argv[1]) as file:
+    def compile(data):
+        regex = re.compile(r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)", re.MULTILINE|re.DOTALL)
+        def _replacer(match):
+            if match.group(2) is None:
+                return match.group(1)
+        return " ".join(regex.sub(_replacer, data).replace("\n",";").replace(":;",": ").replace("{;","{").split()).replace("; ",";")
     with open("compiled.js","a") as f:
-        f.write("(()=>{"+file.read()+"})()")
+        f.write("(()=>{"+compile(file.read())+"})()")
     
